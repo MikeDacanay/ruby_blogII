5a55b7b2549be54b8273c45b5ee990076ec3d379
Mike Dacanay <migd64@gmail.com>
2017-10-06 01:48:38 -0700

    final commit, please look at list_of_commands.txt

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,5 @@ class Blog < ActiveRecord::Base
 	validates :name, :description, presence: true
 	has_many :owners
 	has_many :users, through: :owners
+	has_many :posts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
 	validates :first_name, :last_name, :email_address, presence: true
 	has_many :owners
 	has_many :blogs, through: :owners
+	has_many :posts
+	has_many :messages
 end
