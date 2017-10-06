c6626b054bcc91c36bbaae3abd5148903c6f4508
Mike Dacanay <migd64@gmail.com>
2017-10-06 01:30:38 -0700

    working on #16th query

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,5 @@
 class Blog < ActiveRecord::Base
 	validates :name, :description, presence: true
+	has_many :owners
+	has_many :users, through: :owners
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 	validates :first_name, :last_name, :email_address, presence: true
+	has_many :owners
+	has_many :blogs, through: :owners
 end
